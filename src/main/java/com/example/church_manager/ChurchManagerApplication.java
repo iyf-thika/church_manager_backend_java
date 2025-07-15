ebee313c75592ee690c54ae04dee4df93194e437
Kelvin Mwangi <greenkariuki@gmail.com>
2025-07-16 00:59:11 +0300

    setup of the project

---
+++ src/main/java/com/example/church_manager/ChurchManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.church_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/church_manager/ChurchManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.church_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
